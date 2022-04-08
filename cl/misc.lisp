fd831a89f88713518e4de4af3e310edf5adc1184
digikar99 <shubhamkarayare@gmail.com>
2022-04-08 11:42:34 +0530

    [add] doc for EXTENSIBLE-COMPOUND-TYPES-CL shadowing symbols

--- cl/misc.lisp
+++ cl/misc.lisp
@@ -5,3 +5,7 @@
 
 (cl:deftype excl:function (&optional arg-typespec value-typespec)
   `(cl:function ,arg-typespec ,value-typespec))
+
+(do-external-symbols (s (find-package :extensible-compound-types-cl))
+  (setf (documentation s 'cl:function)
+        (documentation (find-symbol (symbol-name s) :cl) 'cl:function)))
