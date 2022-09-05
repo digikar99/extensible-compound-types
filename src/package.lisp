9984c12384b8669f9974436232646ca06ddcd62a
digikar99 <shubhamkarayare@gmail.com>
2022-09-05 17:28:17 +0530

    [fix] better handle define-declaration args on CCL

--- src/package.lisp
+++ src/package.lisp
@@ -85,7 +85,12 @@
 (in-nomine:define-namespace type)
 
 (define-declaration extype (args)
-  (destructuring-bind (type &rest vars) args
+  ;; On CCL, args starts with DECL-NAME while not using CL-ENVIRONMENTS-CL
+  ;; Other times, it starts with the appropriate args
+  (destructuring-bind (type &rest vars) (optima:match args
+                                          ((list* 'extype args)
+                                           args)
+                                          (_ args))
     (values :variable
             (mapcar (lambda (var)
                       (list var 'extype type))
@@ -93,7 +98,12 @@
 
 #+extensible-compound-types
 (define-declaration type (args)
-  (destructuring-bind (type &rest vars) args
+  ;; On CCL, args starts with DECL-NAME while not using CL-ENVIRONMENTS-CL
+  ;; Other times, it starts with the appropriate args
+  (destructuring-bind (type &rest vars) (optima:match args
+                                          ((list* 'type args)
+                                           args)
+                                          (_ args))
     (values :variable
             (append (mapcar (lambda (var)
                               (list var 'type type))
