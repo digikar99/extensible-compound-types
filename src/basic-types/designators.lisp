328d1cdf8fae8b274589cef522f6d5c7516efe84
digikar99 <shubhamkarayare@gmail.com>
2023-01-22 23:24:36 +0530

    [add] define types for compiler-macro-notes classes

--- src/basic-types/designators.lisp
+++ src/basic-types/designators.lisp
@@ -26,45 +26,6 @@
        function
        (and list (satisfies function-designator-p))))
 
-;; (define-compound-type character-designator (o)
-;;   (or (characterp o)
-;;       (and (stringp o)
-;;            (= 1 (length o)))
-;;       (and (symbolp o)
-;;            (= 1 (length (symbol-name o))))))
-
-;; (macrolet ((def (n2)
-;;              `(progn
-;;                 (defmethod %subtypep ((n1 (eql 'character-designator)) (n2 (eql ',n2))
-;;                                       t1 t2 &optional env)
-;;                   (declare (ignore n1 n2 t1 t2 env))
-;;                   (values nil t))
-;;                 (defmethod %intersect-type-p ((n1 (eql 'character-designator)) (n2 (eql ',n2))
-;;                                               t1 t2 &optional env)
-;;                   (declare (ignore n1 n2 t1 t2 env))
-;;                   (values t t)))))
-;;   (def character)
-;;   (def symbol)
-;;   (def string)
-;;   (def sequence))
-
-;; (defmethod %subtypep ((n1 (eql 'character)) (n2 (eql 'character-designator)) t1 t2
-;;               &optional env)
-;;     (declare (ignore n1 n2 t1 t2 env))
-;;     (values t t))
-
-;; (defmethod %subtypep ((n1 (eql 'character-designator)) (n2 (eql 'or))
-;;                       t1 t2 &optional env)
-;;   (declare (ignore n1 n2 t1))
-;;   (multiple-value-bind (subtypep knownp) (subtypep '(or character symbol string) t2 env)
-;;     (values subtypep knownp)))
-
-;; (defmethod %subtypep ((n1 (eql 'or)) (n2 (eql 'character-designator))
-;;                       t1 t2 &optional env)
-;;   (declare (ignore n1 n2 t2))
-;;   (multiple-value-bind (subtypep knownp) (subtypep t1 '(or character (string 1)) env)
-;;     (values subtypep knownp)))
-
 (5am:def-test character-designator ()
   (5am:is (equal '(nil t) (multiple-value-list (subtypep 'character-designator 'character))))
   (5am:is (equal '(nil t) (multiple-value-list (subtypep 'character-designator 'symbol))))
---
+++ src/basic-types/misc-types.lisp
@@ -0,0 +1,5 @@
+(in-package :extensible-compound-types.impl)
+
+(define-orthogonally-specializing-type compiler-macro-notes:optimization-failure-note
+    () ())
+(define-orthogonally-specializing-type compiler-macro-notes:note () ())
